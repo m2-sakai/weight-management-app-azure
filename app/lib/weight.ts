5f9338ed9f9a8beeefcb7a87fc5795218c946808
NRI酒井 将大 <m2-sakai@nri.co.jp>
2023-12-06 13:51:43 +0900

    user取得処理をまとめ

--- app/lib/weight.ts
+++ app/lib/weight.ts
@@ -2,15 +2,12 @@
 import { unstable_noStore as noStore } from 'next/cache';
 import { Weight } from '@/app/types/Weight';
 import prisma from '@/app/lib/prisma';
+import { getUser } from '@/app/lib/user';
 
 export async function fetchWeightsForCalender(email: string, month: number) {
   noStore();
   try {
-    const user = await prisma.user.findFirst({
-      where: {
-        email: email,
-      },
-    });
+    const user = getUser(email);
     if (user !== null) {
       // カレンダーは前後月も少し表示されるため、3カ月分取得する
       const weight: Weight[] = await prisma.$queryRaw`SELECT * FROM wm_weights WHERE user_id=${
@@ -29,11 +26,7 @@ export async function fetchWeightsForCalender(email: string, month: number) {
 export async function fetchWeightsForGraph(email: string, range: number) {
   noStore();
   try {
-    const user = await prisma.user.findFirst({
-      where: {
-        email: email,
-      },
-    });
+    const user = getUser(email);
     if (user !== null) {
       const day = new Date();
       day.setDate(day.getDate() - range);
@@ -53,11 +46,7 @@ export async function fetchWeightsForGraph(email: string, range: number) {
 export async function registerWeight(email: string, weight: number, date: string) {
   noStore();
   try {
-    const user = await prisma.user.findFirst({
-      where: {
-        email: email,
-      },
-    });
+    const user = getUser(email);
     if (user !== null) {
       await prisma.weight.upsert({
         where: {
