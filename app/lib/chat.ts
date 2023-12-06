809df3202e2a5c7b88bb9211852eda21442642b0
NRI酒井 将大 <m2-sakai@nri.co.jp>
2023-12-07 08:30:46 +0900

    aoaiに移行

--- app/lib/chat.ts
+++ app/lib/chat.ts
@@ -1,28 +1,24 @@
+'use server';
+
 import { Message } from '@/app/types/Message';
-import axios from 'axios';
+import { OpenAIClient, AzureKeyCredential } from '@azure/openai';
+
+const client = new OpenAIClient(
+  process.env.AZURE_OPENAI_ENDPOINT ?? '',
+  new AzureKeyCredential(process.env.AZURE_OPENAI_API_KEY ?? '')
+);
+const modelName = process.env.AZURE_OPENAI_MODEL_NAME ?? '';
 
 export const chat = async (chats: Message[], message: Message): Promise<Message> => {
-  const response = await axios.post(
-    'https://api.openai.com/v1/chat/completions',
-    {
-      model: 'gpt-3.5-turbo',
-      messages: [...chats, message].map((d) => ({
-        role: d.role,
-        content: d.content,
-      })),
-    },
-    {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`,
-      },
-      // timeout: 20000,
-    }
-  );
+  const messages = [...chats, message].map((d) => ({
+    role: d.role,
+    content: d.content,
+  }));
 
-  const data = await response.data;
-  if (response.status !== 200) {
-    throw data.error || new Error(`Request failed with status ${response.status}`);
+  try {
+    const response = await client.getChatCompletions(modelName, messages);
+    return response.choices[0].message as Message;
+  } catch (error) {
+    throw new Error('Chat Error error: ' + error);
   }
-  return data.choices[0].message as Message;
 };
